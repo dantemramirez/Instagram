8ea1ce8759457d1c8af4a61e1f3e954abd6ff3d4
dantemramirez <Dante.ramirez@ttu.edu>
2021-10-11 15:31:44 -0500

    Insta1

--- Instagram/AppDelegate.swift
+++ Instagram/AppDelegate.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import Parse
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -14,6 +15,15 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        let parseConfig = ParseClientConfiguration
+        {
+            $0.applicationId = "r4hPfFQi0Spq1w8mFgf6o2tcrR79y7KDdxnBvgVc"
+            $0.clientKey = "pGFtSzBmyocNsuhlpsogVSUPcknBJiBK0F9RVdnM"
+            $0.server = "https://parseapi.back4app.com"
+        }
+        Parse.initialize(with: parseConfig)
+        
         return true
     }
 
---
+++ Instagram/PostCell.swift
@@ -0,0 +1,28 @@
+//
+//  PostCell.swift
+//  Instagram
+//
+//  Created by Dante Ramirez on 10/8/21.
+//
+
+import UIKit
+
+class PostCell: UITableViewCell {
+    @IBOutlet weak var photoView: UIImageView!
+    @IBOutlet weak var usernameLabel: UILabel!
+    @IBOutlet weak var captionLabel: UILabel!
+    @IBOutlet weak var topUsernameLabel: UILabel!
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
